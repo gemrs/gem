bc89b51e517b15eae0804a73484dbecccbf4dc40
T Reznik <sinusoids@gmx.com>
2015-08-18 20:05:16 +0000

    Pythonic interface to tasks

--- content/plugins/sample_plugin/sample.py
+++ content/plugins/sample_plugin/sample.py
@@ -1,19 +1,28 @@
 import gem.task
 import plugins.gem_plugin as plugins
+import task
+
+class SampleTask(task.Task):
+    def __init__(self, count, interval=1):
+        super(SampleTask, self).__init__(interval)
+        self.count = count
+
+    def execute(self):
+        logger.Info("Tock.. {0}".format(self.count))
+        self.count -= 1
+        return self.count > 0
 
 class SamplePlugin(plugins.GemPlugin):
-    count = 10
+    sample_task = SampleTask(10)
+
     def startup(self, event):
+        global logger
+        logger = self.logger
         self.logger.Info("Startup hook")
-        gem.task.submit(self.task_test, gem.task.PostTick, 2, None)
+        self.sample_task.submit()
 
     def shutdown(self, event):
         self.logger.Info("Shutdown hook")
 
     def tick(self, event):
         self.logger.Info("Tick")
-
-    def task_test(self, when, userdata):
-        self.logger.Info("Tock.. {0}".format(self.count))
-        self.count -= 1
-        return self.count > 0
---
+++ content/task.py
@@ -0,0 +1,23 @@
+import gem
+import gem.task
+
+from abc import ABCMeta, abstractmethod
+
+logger = gem.syslog.Module(__name__)
+
+class Task(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, interval, when=gem.task.Tick):
+        self.interval = interval
+        self.when = when
+
+    def submit(self):
+        gem.task.submit(self.__execute__, self.when, self.interval, None)
+
+    def __execute__(self, task, userdata):
+        return self.execute()
+
+    @abstractmethod
+    def execute(self):
+        pass
