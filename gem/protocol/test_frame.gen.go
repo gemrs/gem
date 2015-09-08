e2708f11b974e5709ef32a588c710799b217e37a
T Reznik <sinusoids@gmx.com>
2015-09-08 23:00:30 +0000

    framecc: output formatting

--- gem/protocol/test_frame.gen.go
+++ gem/protocol/test_frame.gen.go
@@ -2,41 +2,47 @@
 package protocol
 
 import (
-	"github.com/sinusoids/gem/gem/encode"
+	"github.com/sinusoids/gem/gem/encoding"
 )
 
+type TestFrame AnonStruct_45
+
 type EmbeddedStruct struct {
-	A encode.Int32
-	B encode.Int32
-	C encode.Int32
+	A encoding.Int32
+	B encoding.Int32
+	C encoding.Int32
 }
 
-func (struc *EmbeddedStruct) Encode(buf *bytes.Buffer, flags_ interface{}) (err error) {
-	err = struc.A.Encode(buf, encode.IntegerFlag(encode.IntLittleEndian))
+func (struc *EmbeddedStruct) Encode(buf *bytes.Buffer, flags interface{}) (err error) {
+	err = struc.A.Encode(buf, encoding.IntegerFlag(encode.IntLittleEndian))
 	if err != nil {
 		return err
 	}
-	err = struc.B.Encode(buf, encode.IntegerFlag(encode.IntOffset128|encode.IntPDPEndian))
+
+	err = struc.B.Encode(buf, encoding.IntegerFlag(encode.IntOffset128|encode.IntPDPEndian))
 	if err != nil {
 		return err
 	}
-	err = struc.C.Encode(buf, encode.IntegerFlag())
+
+	err = struc.C.Encode(buf, encoding.IntegerFlag())
 	if err != nil {
 		return err
 	}
 
 }
 
-func (struc *EmbeddedStruct) Decode(buf *bytes.Buffer, flags_ interface{}) (err error) {
-	err = struc.A.Decode(buf, encode.IntegerFlag(encode.IntLittleEndian))
+func (struc *EmbeddedStruct) Decode(buf *bytes.Buffer, flags interface{}) (err error) {
+	err = struc.A.Decode(buf, encoding.IntegerFlag(encode.IntLittleEndian))
 	if err != nil {
 		return err
 	}
-	err = struc.B.Decode(buf, encode.IntegerFlag(encode.IntOffset128|encode.IntPDPEndian))
+
+	err = struc.B.Decode(buf, encoding.IntegerFlag(encode.IntOffset128|encode.IntPDPEndian))
 	if err != nil {
 		return err
 	}
-	err = struc.C.Decode(buf, encode.IntegerFlag())
+
+	err = struc.C.Decode(buf, encoding.IntegerFlag())
 	if err != nil {
 		return err
 	}
@@ -44,58 +50,62 @@ func (struc *EmbeddedStruct) Decode(buf *bytes.Buffer, flags_ interface{}) (err 
 }
 
 type AnonStruct_45 struct {
-	Message  encode.JString
-	Values8  [4]encode.Int16
-	Values16 [2]encode.Int16
+	Message  encoding.JString
+	Values8  [4]encoding.Int16
+	Values16 [2]encoding.Int16
 	Struc    EmbeddedStruct
 }
 
-func (struc *AnonStruct_45) Encode(buf *bytes.Buffer, flags_ interface{}) (err error) {
+func (struc *AnonStruct_45) Encode(buf *bytes.Buffer, flags interface{}) (err error) {
 	err = struc.Message.Encode(buf, 256)
 	if err != nil {
 		return err
 	}
+
 	for i := 0; i < 4; i++ {
-		err = struc.Values8[i].Encode(buf, encode.IntegerFlag())
+		err = struc.Values8[i].Encode(buf, encoding.IntegerFlag())
 		if err != nil {
 			return err
 		}
 	}
+
 	for i := 0; i < 2; i++ {
-		err = struc.Values16[i].Encode(buf, encode.IntegerFlag())
+		err = struc.Values16[i].Encode(buf, encoding.IntegerFlag())
 		if err != nil {
 			return err
 		}
 	}
-	err = struc.Struc.Encode(buf, encode.NilFlags)
+
+	err = struc.Struc.Encode(buf, encoding.NilFlags)
 	if err != nil {
 		return err
 	}
 
 }
 
-func (struc *AnonStruct_45) Decode(buf *bytes.Buffer, flags_ interface{}) (err error) {
+func (struc *AnonStruct_45) Decode(buf *bytes.Buffer, flags interface{}) (err error) {
 	err = struc.Message.Decode(buf, 256)
 	if err != nil {
 		return err
 	}
+
 	for i := 0; i < 4; i++ {
-		err = struc.Values8[i].Decode(buf, encode.IntegerFlag())
+		err = struc.Values8[i].Decode(buf, encoding.IntegerFlag())
 		if err != nil {
 			return err
 		}
 	}
+
 	for i := 0; i < 2; i++ {
-		err = struc.Values16[i].Decode(buf, encode.IntegerFlag())
+		err = struc.Values16[i].Decode(buf, encoding.IntegerFlag())
 		if err != nil {
 			return err
 		}
 	}
-	err = struc.Struc.Decode(buf, encode.NilFlags)
+
+	err = struc.Struc.Decode(buf, encoding.NilFlags)
 	if err != nil {
 		return err
 	}
 
 }
-
-type TestFrame AnonStruct_45
