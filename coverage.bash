0f901fea666bfbeb6fa0825b41715a4ad3ef2768
T Reznik <sinusoids@gmx.com>
2015-09-29 22:59:26 +0000

    Add script to generate coverage profile

---
+++ coverage.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+cd src
+go get github.com/wadey/gocovmerge
+packages=`find gem bbc -type f -name "*.go" -printf "%h\n" | sort | uniq | grep -v vendor`
+covfiles=""
+for f in $packages; do
+  go test -v -coverprofile $f/coverage.profile $f
+  [ -f $f/coverage.profile ] && covfiles+="$f/coverage.profile "
+done
+gocovmerge $covfiles > ../coverage.profile
+cd -
