2a90ff8cbd9e01acd04aabd3fbe02f675a3c3963
T Reznik <sinusoids@gmx.com>
2015-10-26 17:56:15 +0000

    test.sh: ensure we set a non-zero exit code if a test fails

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RETURN_CODE=0
+
 test_package() {
     root_pkg=$(go list $1)
     root_profile=$2
@@ -8,6 +10,7 @@ test_package() {
         profile_path=$(echo $pkg | sed 's/\//_/g')
         profile_path+=".profile"
         go test -coverpkg=${root_pkg}/... -covermode=count -coverprofile=${profile_path} ${pkg}
+        [ $? -ne 0 ] && RETURN_CODE=1
         [[ -f ${profile_path} ]] && all_profiles+=" ${profile_path}"
     done
 
@@ -19,3 +22,4 @@ test_package ./gem gem.profile
 test_package ./bbc bbc.profile
 gocovmerge gem.profile bbc.profile > coverage.profile
 rm gem.profile bbc.profile # Remove the old profiles to avoid confusion
+exit $RETURN_CODE
